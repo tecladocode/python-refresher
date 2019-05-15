825b924ace1719f17dc17b12546db1258f879833
Jose Salvatierra <jslvtr@gmail.com>
2019-05-16 00:14:11 +0200

    Add lecture on user input which means moving all other lectures up by 1.

---
+++ 3_getting_user_input/code.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+print(name)
+
+# -- Mathematics on user input --
+
+size_input = input("How big is your house (in square feet): ")
+square_feet = int(size)
+square_metres = square_feet * 15  # Make sure this is correct
+print(f"{square_feet} square feet is {square_metres} square metres.")
