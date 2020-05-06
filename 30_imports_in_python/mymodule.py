a906c30eef47e53cb13d621061b9ccc47e42b36c
neelmani-pathak <64878562+neelmani-pathak@users.noreply.github.com>
2020-05-05 21:25:33 -0500

    Update mymodule.py

--- 30_imports_in_python/mymodule.py
+++ 30_imports_in_python/mymodule.py
@@ -1,3 +1,6 @@
+import libs.mylib
+
+
 def divide(dividend, divisor):
     return dividend / divisor
 
@@ -6,6 +9,5 @@ print("mymodule.py:", __name__)
 
 # -- importing from a folder --
 
-import libs.mylib
 
 print("mymodule.py:", __name__)
